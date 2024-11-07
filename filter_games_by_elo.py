60b03b3e25149675e4ffb20561f036aca0960460
Juan Ortega <jportega02@gmail.com>
2024-11-07 17:09:53 -0300

    Increased data per file, prepared training for single file
    Changed the ammount of features and leabels per file from 500000 to 2kk. Changed model_training accordingly to only use one file as a train dataloader. Im going to go through the files one at a time.

--- filter_games_by_elo.py
+++ filter_games_by_elo.py
@@ -1,6 +1,8 @@
 import chess.pgn
 
 def filter_games(pgn_file, output_file, min_elo):
+    total_games = 0
+    filtered_games = 0
     with open(pgn_file) as games_file:
         with open(output_file, 'w') as out_file:
             while True:
@@ -11,9 +13,15 @@ def filter_games(pgn_file, output_file, min_elo):
                 white_elo = int(game.headers.get("WhiteElo", 0))
                 black_elo = int(game.headers.get("BlackElo", 0))
                 avg_elo = (white_elo + black_elo) / 2
-
+                total_games += 1
+                
                 if avg_elo >= min_elo:
+                    filtered_games += 1
                     # write game to out file
                     out_file.write(str(game) + "\n\n")
+                
+                if total_games % 1000 == 0:
+                    print(f"total games: {total_games} \nfiltered games: {filtered_games}")
+                    print(f"% of filtered games = {((filtered_games * 100) / total_games):.2f}%")
 
-filter_games("lichess_2020_oct.pgn", "lichess_2020_oct_filtered.pgn", min_elo=2000)
+filter_games("lichess_db_standard_rated_2020-10.pgn", "lichess_2020_oct_filtered.pgn", min_elo=2000)
--- model_training.py
+++ model_training.py
@@ -1,11 +1,9 @@
 import numpy as np
-import pickle
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
 import os
-import glob
 from chess_model_v1 import *
 
 
@@ -38,162 +36,82 @@ class EarlyStopping:
 
 
 class ChessDataset(Dataset):
-    def __init__(self, file_prefix, specific_file):
-        if not specific_file:
-            self.file_list = sorted(glob.glob(f"{file_prefix}_*.npz"))
-        else:
-            self.file_list = [specific_file]
-        self.file_sizes = []
-        self.total_samples = 0
+    def __init__(self, file_path, transform=None):
+        data = np.load(file_path)
+        self.features = data["features"]
+        self.labels = data["labels"]
 
-        for file_path in self.file_list:
-            data = np.load(file_path)
-            num_samples = len(data['labels'])
-            self.file_sizes.append(num_samples)
-            self.total_samples += num_samples
-
-        self.current_file_index = 0
-        self.current_data = None
-        self.load_file(self.current_file_index)
-
-
-    def reset(self):
-        # resets dataset at the beginning of each epoch
-        self.current_file_index = 0
-        self.features = None
-        self.labels = None
-        self.load_file(self.current_file_index)
-
-
-    def load_file(self, file_index):
-        # load file given the index
-        file_path = self.file_list[file_index]
-        print(f"loading data from {file_path}")
-        loaded_data = np.load(file_path)
-        self.features = loaded_data["features"]
-        self.labels = loaded_data["labels"]
-        self.current_file_size = len(self.labels) # number of samples
-
-
+        self.transform = transform
 
     def __len__(self):
-        return self.total_samples
+        return len(self.labels)
     
     def __getitem__(self, idx):
-        if idx >= self.total_samples:
-            raise IndexError("index out of range")
         
-        
-        # determine the file and local index inside that file
-        file_index = 0
-        total_size = 0
-        for size in self.file_sizes:
-            if idx < total_size + size:
-                break
-            total_size += size
-            file_index += 1
+        board_state = self.features[idx]
+        move_label = self.labels[idx]
+
+        if self.transform:
+            board_state = self.transform(board_state)
+
+        board_state_tensor = torch.tensor(board_state, dtype=torch.float32)
+        move_label_tensor = torch.tensor(move_label, dtype=torch.float32)
 
 
 
-        if file_index != self.current_file_index:
-            self.current_file_index = file_index
-            self.load_file(self.current_file_index)
-
-        in_file_index = idx - total_size
-
-        feature = self.features[in_file_index]
-        label = self.labels[in_file_index]
-
-        return torch.tensor(feature, dtype=torch.float32), torch.tensor(label, dtype=torch.float32)
+        return board_state_tensor, move_label_tensor
     
 
     
 
-def train_model(model, train_loader, val_loader, save_path, num_epochs=100, learning_rate=0.001,  batch_size=64):
+def train_model(model, train_loader, num_epochs, learning_rate=0.001,  batch_size=64):
 
     model.train()
     loss_fn = nn.MSELoss() # mean squared error loss
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-    early_stopping = EarlyStopping(patience=3, delta=0, save_path=save_path)
+    #early_stopping = EarlyStopping(patience=3, delta=0, save_path=save_path)
 
     for epoch in range(num_epochs):
- 
+        
+        total_loss = 0.0
         model.train()
-        total_loss = 0
 
         for batch_index, (features, labels) in enumerate(train_loader):
-            if batch_index >= 1000:
-                break
-            if len(features) == 0:
-                print("no more data to train")
-                break
-
             features = features.to(device)
             labels = labels.to(device)
 
-            # reshape input tensor to (batch_size, channels, height, width)
-            features = features.permute(0,3,1,2)
-
             optimizer.zero_grad()
 
-            outputs = model(features) 
+            outputs = model(features)
 
-            loss = loss_fn(outputs.view(-1), labels)
-            total_loss += loss.item()
+            loss = loss_fn(outputs.squeeze(), labels)
 
             loss.backward()
 
             optimizer.step()
 
-            if batch_index % 100 == 0:
-                print(f'Epoch [{epoch + 1}/{num_epochs}], batch [{batch_index}], loss: {loss.item():.4f}')
+            total_loss += loss.item()
 
-        avg_loss = total_loss / len(train_loader)
-        print(f"epoch: [{epoch+1} / {num_epochs}], loss: {avg_loss:.4f}")
-        print(f"model saved to {save_path}")
+            if batch_index % 100 == 99:
+                print(f"Epoch: [{epoch+1}/{num_epochs}], Batch: [{batch_index+1}], Loss: [{total_loss / 100:.4f}]")
+                total_loss = 0.0
 
-        model.eval()
-        val_loss = 0.0
-        with torch.inference_mode():
-            for features, labels in val_loader:
-                features = features.permute(0,3,1,2).to(device)
-                labels = labels.to(device)
-                outputs = model(features)
-                loss = loss_fn(outputs.view(-1), labels)
-                val_loss += loss.item()
+        torch.save(model.state_dict(), f"chess_model_epoch")
 
-        avg_val_loss = val_loss / len(val_loader)
-        print(f"Epoch {epoch+1} Validation Loss: {avg_val_loss:.4f}")
-
-        early_stopping(avg_val_loss, model)
-
-        if early_stopping.early_stop:
-            print("early stopping triggered, exiting training loop")
-            break
-
-        model.train()
+    print("training complete!")
 
 if torch.cuda.is_available():
     device = "cuda"
 else:
     device = "cpu"
 
-save_path = "chess_model_v1.pth"
-model = chess_model_v1().to(device)
+npz_file = "data_path.npz"
+dataset = ChessDataset(npz_file)
 
-if os.path.isfile(save_path):
-    print(f"loading model from {save_path}")
-    model.load_state_dict(torch.load((save_path), map_location=device))
+train_loader = DataLoader(dataset, batch_size=64, shuffle=True)
 
-else:
-    print(f"no model found at {save_path}. starting from zero")
+model = chess_model_v1.to(device)
 
-file_prefix = "processed_dataset"
-train_dataset = ChessDataset(file_prefix, specific_file=False)
-val_dataset = ChessDataset(file_prefix= False, specific_file="val_dataset.npz")
-train_loader = DataLoader(dataset=train_dataset, batch_size=256, shuffle=False)
-val_loader = DataLoader(dataset=val_dataset, batch_size=256, shuffle=False)
-
-train_model(model, train_loader, val_loader, save_path="chess_model_v1.pth", num_epochs=50, learning_rate=0.001, batch_size=256)
+train_model(model, train_loader, num_epochs=10, learning_rate=0.001, batch_size=64)
 
